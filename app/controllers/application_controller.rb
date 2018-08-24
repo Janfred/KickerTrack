1cdfa286bd9959f37fa6754e4e243c94418697f7
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2018-08-24 23:56:57 +0200

    devise user, cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    if can? :read, exception.subject
+      redirect_to exception.subject, :alert => exception.message
+    else
+      redirect_to root_url, :alert => exception.message
+    end
+  end
 end
