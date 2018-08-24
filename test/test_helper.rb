3cc2715e22c3276cc303ac407cf5e5e1f614c3fa
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2018-08-24 23:31:00 +0200

    Cucumber, Simplecov, Codacy-Coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'simplecov'
+if ENV['CODACY_PROJECT_TOKEN']
+  require 'codacy-coverage'
+  Codacy::Reporter.start
+end
+SimpleCov.start
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
